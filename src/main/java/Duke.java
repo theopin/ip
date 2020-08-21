9cd8eb5cc9ce25c7e4fe9101038af3f6741958fb
theopin <theopinto98@gmail.com>
2020-08-21 20:40:44 +0800

    Add echo functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,28 +1,52 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        boolean isExit = false;
         printWelcomeText();
+
+        do {
+            isExit = insertCommand();
+        }
+        while(!isExit);
+
         printExitText();
     }
 
     public static void printWelcomeText() {
         printHorizontalLine();
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(" Hello from\n" + logo);
-        System.out.println(" What can I do for you?");
+        String logo = "\t ____        _        \n"
+                + "\t|  _ \\ _   _| | _____ \n"
+                + "\t| | | | | | | |/ / _ \\\n"
+                + "\t| |_| | |_| |   <  __/\n"
+                + "\t|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("\tHello from\n" + logo);
+        System.out.println("\tWhat can I do for you?");
+        printHorizontalLine();
+    }
+
+    public static boolean insertCommand() {
+        String userCommand;
+        Scanner myCommand = new Scanner(System.in);
+        userCommand = myCommand.nextLine();
+        printHorizontalLine();
+
+
+        if (!userCommand.equals("bye")) {
+            System.out.println("\t" + userCommand);
+            printHorizontalLine();
+            return false;
+        }
+        return true;
     }
 
     public static void printExitText() {
-        printHorizontalLine();
-        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("\tBye. Hope to see you again soon!");
         printHorizontalLine();
     }
 
     public static void printHorizontalLine() {
-        String horizontalLine = "____________________________________________________________";
+        String horizontalLine = "\t____________________________________________________________";
         System.out.println(horizontalLine);
     }
 }
