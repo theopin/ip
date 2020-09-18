cd986d130e7159fe88a63cbab2e94927d1537407
theopin <theopinto98@gmail.com>
2020-09-18 21:31:59 +0800

    Shift parser related methods to CommandParser class

--- src/main/java/duke/UserSession.java
+++ src/main/java/duke/UserSession.java
@@ -7,6 +7,7 @@ import duke.exception.IllegalCommandException;
 import duke.exception.PartialCommandException;
 import duke.exception.RangeExceedException;
 import duke.message.Message;
+import duke.parser.CommandParser;
 import duke.task.*;
 
 import java.util.Arrays;
@@ -20,18 +21,10 @@ public class UserSession {
     // String Constants
 
     public static final String ACTION_LIST = "list";
-    public static final String ACTION_DONE = "done";
     public static final String ACTION_TODO = "todo";
     public static final String ACTION_EVENT = "event";
     public static final String ACTION_DEADLINE = "deadline";
     public static final String ACTION_EXIT = "bye";
-    public static final String ACTION_REMOVE = "remove";
-    public static final String WHITESPACE = " ";
-
-    private static final String[] taskTypes = {ACTION_TODO, ACTION_EVENT, ACTION_DEADLINE};
-    private static final String[] standaloneCommand = {ACTION_EXIT, ACTION_LIST};
-
-    //public static ArrayList<Task> tasks = new ArrayList<>();
 
     public UserSession() {
         new ReadDataFile();
@@ -41,12 +34,14 @@ public class UserSession {
     // Overall structure of a user session
     public void runProgramSequence() {
         String action = "";
+        CommandParser newUserCommand;
         Message.printWelcomeText();
 
         // Ask for new user input until user types an exit command
         do {
             try {
-                action = requestUserInput();
+                newUserCommand = new CommandParser();
+                action = newUserCommand.getActionCommand();
             } catch (IllegalCommandException e) {
                 e.alertException();
             } catch (PartialCommandException p) {
@@ -57,30 +52,4 @@ public class UserSession {
         Message.printExitText();
     }
 
-    public String requestUserInput() throws IllegalCommandException, PartialCommandException {
-        String[] userInput = receiveUserInput();
-        userInput[0] = userInput[0].toLowerCase();
-        new TaskHandler(userInput);
-        //new TaskHandler
-        return userInput[0];
-    }
-
-    // Takes in input from the user
-    public String[] receiveUserInput() throws IllegalCommandException, PartialCommandException {
-        String userCommand = myScanner.nextLine().trim();
-        Message.printHorizontalLine();
-        String[] splitCommand = userCommand.split(WHITESPACE);
-
-        if (!(Arrays.asList(standaloneCommand).contains(splitCommand[0]))) {
-            if (Arrays.asList(taskTypes).contains(splitCommand[0])) {
-                throw new PartialCommandException(splitCommand[0]);
-            } else {
-                throw new IllegalCommandException();
-            }
-        }
-        return splitCommand;
-    }
-
 }
-
-
---
+++ src/main/java/duke/parser/CommandParser.java
@@ -0,0 +1,63 @@
+package duke.parser;
+
+import duke.exception.IllegalCommandException;
+import duke.exception.PartialCommandException;
+import duke.message.Message;
+import duke.task.TaskHandler;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static duke.UserSession.*;
+
+public class CommandParser {
+    // Scanner for user input
+    private static final Scanner myScanner = new Scanner(System.in);
+
+    public static final String WHITESPACE = " ";
+
+    private static final String[] taskTypes = {ACTION_TODO, ACTION_EVENT, ACTION_DEADLINE};
+    private static final String[] standaloneCommand = {ACTION_EXIT, ACTION_LIST};
+
+    private String actionCommand;
+    //public static ArrayList<Task> tasks = new ArrayList<>();
+
+    public CommandParser() throws IllegalCommandException, PartialCommandException {
+        requestUserInput();
+    }
+
+
+
+    public void requestUserInput() throws IllegalCommandException, PartialCommandException {
+        String[] userInput = receiveUserInput();
+        userInput[0] = userInput[0].toLowerCase();
+        new TaskHandler(userInput);
+        setActionCommand(userInput[0]);
+    }
+
+    // Takes in input from the user
+    public String[] receiveUserInput() throws IllegalCommandException, PartialCommandException {
+        String userCommand = myScanner.nextLine().trim();
+        Message.printHorizontalLine();
+        String[] splitCommand = userCommand.split(WHITESPACE);
+
+        if (!(Arrays.asList(standaloneCommand).contains(splitCommand[0]))) {
+            if (Arrays.asList(taskTypes).contains(splitCommand[0])) {
+                throw new PartialCommandException(splitCommand[0]);
+            } else {
+                throw new IllegalCommandException();
+            }
+        }
+        return splitCommand;
+    }
+
+    public void setActionCommand(String actionCommand) {
+        this.actionCommand = actionCommand;
+    }
+
+    public String getActionCommand() {
+        return actionCommand;
+    }
+}
+
+
